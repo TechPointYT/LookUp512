2d84b36e6694cce04ccd8be0fcdf5cf75767df84
Jack Thielman <jack.thielman@gmail.com>
2020-11-11 20:41:02 -0600

    Restaurant-first location
    This is more or less the complete version of one restaurant entry. The rest should mostly be copy paste.

--- maps.js
+++ maps.js
@@ -1,13 +1,14 @@
 function initMap() {
-	const austin = { lat: 30.2672, lng: -97.7431 };
+	const austin = {lat: 30.2672, lng: -97.7431};
 
 	const map = new google.maps.Map(document.getElementById("map"), {
-	  zoom: 12,
-	  center: austin,
+		zoom: 13,
+		center: austin,
 	});
 
-	/*const marker = new google.maps.Marker({
-	  position: austin,
-	  map: map,
-	});*/
+	const UT = new google.maps.Marker({
+		position: austin,
+		map: map,
+		label: "UT",
+	});
 }
