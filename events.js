3f190892a0e6465755d95519fed9b28208300f52
TechPointYT <david.alvarez.ou@hotmail.com>
2020-11-11 20:18:33 -0600

    Events Page

---
+++ events.js
@@ -0,0 +1,22 @@
+function DemoEvent(){
+	return `
+
+		<div class="item">
+			<h4 class="eventTitle">Event Name</h4>
+			<p class="eventDate">11/11/20</p>
+			<p class="eventDesc">Demo Event description</p>
+			<p ><a class="eventLink" href="#">Link</a></p>
+		</div>
+	`;
+}
+
+$(document).ready(function(){
+	addEvents();
+});
+
+function addEvents(){
+	for (var i = 0; i < 10; i++) {
+		$(".columns").append(DemoEvent());
+	}
+	
+}
