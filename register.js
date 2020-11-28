a1c06560f66d3104d72e244170c97c9aa29764ee
Jack Thielman <jack.thielman@gmail.com>
2020-11-28 02:45:37 -0600

    Initial Backend for login
    Users now register to and are checked from an SQL database. Still need to actually add session stuff, set cookies, etc. and add database connections to other pages.

---
+++ register.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$("#registerForm").submit(function(e){
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "register.php",
+			data: $(this).serialize(),
+			success: function(response){
+				var responseData = JSON.parse(response);
+				
+				if(responseData.success=="1"){
+					window.location.replace("index.html");
+				}
+				else{
+					if(responseData.success=="2"){
+						window.alert(responseData.displayMessage);
+					}
+					else{
+						window.alert("Error: AJAX Response from PHP");
+					}
+				}
+			}
+		});
+	});
+});
--- register.php
+++ register.php
@@ -1,25 +1,42 @@
 <?php
+	require 'sqlInfo.php';
 
-	// Optionally store the parameters in variables
-	$server = "fall-2020.cs.utexas.edu";
-	$user = "cs329e_bulko_lpin3397";
-	$pwd = "canada2Betray2Cervix";
-	$dbName = "cs329e_bulko_lpin3397";
-	$mysqli = new mysqli ($server,$user,$pwd,$dbName);
-	// If it returns a non-zero error number, print a
-	// message and stop execution immediately
-	if ($mysqli->connect_errno) {
-		die('Connect Error: ' . $mysqli->connect_errno . ": " . $mysqli->connect_error);
+	if(!empty($_POST['username'])&&!empty($_POST['password'])){
+		$mysqli = new mysqli($sqlServer,$sqlUsername,$sqlPassword,$sqlDatabase);
+		if($mysqli->connect_errno){
+			die('Connect Error: '.$mysqli->connect_errno.": ".$mysqli->connect_error);
+		}
+		$u = $_POST['username'];
+		$p = $_POST['password'];
+		$message = "Error";
+		$query = "SELECT * from lu512Login WHERE username = '$u';";
+		$result = $mysqli->query($query);
+		if(!$result){
+			die("Query failed: ($mysqli->error <br> SQL command	= $query");
+		}
+		else{
+			$info = mysqli_fetch_all($result);
+			if(!empty($info)){
+				$message = "User already registered. Please try a different username.";
+				echo json_encode(array("success" => 2, "displayMessage" => $message));
+			}
+			else{
+				$query = "INSERT INTO lu512Login(username, password) VALUES('$u', '$p');";
+				if(!mysqli_query($mysqli, $query)){
+					echo "Error: ".$query."".mysqli_error($mysqli);
+				}
+				else{
+					$message = "New user registered";
+					echo json_encode(array("success" => 1, "displayMessage" => $message));
+				}
+			}
+		}
+		if($message=="Error"){
+			$message = "Error Registering";
+			echo json_encode(array("success" => 2, "displayMessage" => $message));
+		}
 	}
-
-	// Create the query as a string
-	$command = "SELECT * FROM patrons WHERE
-	lastName = \"Eilish\"";
-	// Issue the query
-	$result = $mysqli->query($command);
-	// Verify the result
-	if (!$result) {
-		die("Query failed: ($mysqli->error <br> SQL command = $command");
+	else{
+		echo json_encode(array("success" => 0));
 	}
-
 ?>
